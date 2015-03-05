55f54971afdea871ce65e708a35bce68436fe527
timelyportfolio <kent.russell@timelyportfolio.com>
2015-03-04 21:48:43 -0600

    start working on interactively exploring by adding lattice density

--- code.R
+++ code.R
@@ -41,3 +41,31 @@ sp5 %>>%
   treewidget #%>>%
   #htmlwidgets::as.iframe(file="index.html",selfcontained=F,libdir = "./lib")
 
+library(lattice)
+sp5 %>>%
+  # dplyr doesn't like xts, so make a data.frame
+  (
+    data.frame(
+      date = index(.)
+      ,price = .[,1,drop=T]
+    )
+  ) %>>%
+  # add a column for Year
+  mutate( year = as.numeric(format(date,"%Y"))) %>>%
+  # group by our new Year column
+  group_by( year ) %>>%
+  # within each year, find what day in the year so we can join
+  mutate( pos = rank(date) ) %>>%
+  mutate( roc = price/lag(price,k=1) - 1 ) %>>%
+  # can remove date
+  select( -c(date,price) ) %>>%
+  as.data.frame %>>%
+  # years as columns as pos as row
+  spread( year, roc ) %>>%
+  # remove last year since assume not complete
+  ( .[,-ncol(.)] ) %>>%
+  # remove pos since index will be same
+  # select( -pos ) %>>%
+  # fill nas with previous value
+  na.fill(0) %>>% as.data.frame %>>% gather(year,roc,-pos) %>>%
+  (densityplot(~roc,groups=year,data = .))
